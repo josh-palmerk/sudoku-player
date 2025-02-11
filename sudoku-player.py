0f0ec87940282473a4abf61ae04352d3742d5049
Josh Palmer <91572133+josh-palmerk@users.noreply.github.com>
2025-02-10 22:04:10 -0700

    THEY PASSED LETS GO

--- sudoku-player.py
+++ sudoku-player.py
@@ -2,8 +2,8 @@
 import json
 
 def main():
-    test()
-    
+    #test_parse()
+    test_is_move_valid()
     board = get_board_from_file()
     new_board = play_game(board)
     quit_game(new_board)
@@ -97,7 +97,7 @@ def prompt_move(board: list) -> list:
 def update_board(move: list, board: list) -> list:
     space = move[0]
     num = move[1]
-    board[space[1]][space[0]] = num
+    board[space[0]][space[1]] = num
     return board
 
 
@@ -134,8 +134,8 @@ def is_move_valid(move, board):
         return (True, "Clearing space.")
  
     #  Step 3: Check if the cell is already occupied 
-    if board[row][col] != 0: 
-        return (False, "This space is already occupied. Choose an empty space.") 
+    #if board[row][col] != 0: 
+        #return (False, "This space is already occupied. Choose an empty space.") 
  
     #  Step 4: Check if the number is valid in the current row 
     if not check_row(row, num, board): 
@@ -191,9 +191,11 @@ def check_col(col, num, board):
  
 def check_box(row, col, num, board): 
     #  Calculate the top-left corner of the 3x3 sub-box 
-    start_row = (row // 3) * 3 
-    start_col = (col // 3) * 3 
- 
+    start_row = row - (row % 3)
+    print(start_row)
+    start_col = col - (col % 3) 
+    print(start_col)
+
     #  Loop through the 3x3 sub-box 
     for i in range(start_row , start_row + 3): 
         for j in range(start_col, start_col + 3): 
@@ -247,17 +249,37 @@ def parse_input(move: str) -> tuple:
 
 #Test functions
 
-def test():
+def test_is_move_valid():
+    board = []
+    with open("hard.json", 'r') as file:
+        board = json.load(file)['board']
+    
+    assert is_move_valid(((0, 0), 1), board)[0] == False, "Box"
+    assert is_move_valid(((0, 0), 5), board)[0] == False, "Col"
+    assert is_move_valid(((0, 0), 2), board)[0] == True,  "Valid"
+    assert is_move_valid(((4, 4), 1), board)[0] == False, "Row and col"
+    assert is_move_valid(((4, 4), 8), board)[0] == True,  "Valid"
+    assert is_move_valid(((2, 2), 5), board)[0] == False, "Square filled"
+    assert is_move_valid(((0, 8), 8), board)[0] == False, "Box"
+    #assert is_move_valid(((0, 0), 1), board) == False
+    print("All cases passed for is_valid_move()")
+
+
+
+
+
+
+def test_parse():
     print("\nparse A1")
     print(parse_input("A1"))
     print("\nparse B2")
     print(parse_input("B2"))
     print("\nparse hello")
     print(parse_input("hello"))
-    print("\nparse C3")
-    print(parse_input("C3"))
-    print("\nparse D4")
-    print(parse_input("D4"))
+    print("\nparse C2")
+    print(parse_input("C2"))
+    print("\nparse D5")
+    print(parse_input("D5"))
     print("\nparse I9")
     print(parse_input("I9"))
     print("\nparse J0")
@@ -266,7 +288,12 @@ def test():
     print(parse_input("11"))
     print("\nparse BB")
     print(parse_input("BB"))
-
+    print("\nparse 1A")
+    print(parse_input("1A"))
+    print("\nparse 2C")
+    print(parse_input("2C"))
+    print("\nparse 1a")
+    print(parse_input("1a"))
 
 if __name__ == "__main__":
     main()
